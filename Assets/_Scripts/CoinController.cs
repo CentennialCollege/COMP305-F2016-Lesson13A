8ec869eff8f3b8dc2366f3240c1b3272d58115c5
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2016-12-10 11:03:36 -0500

    Initial Commit

---
+++ Assets/_Scripts/CoinController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+
+
+	public AudioSource CoinSound;
+
+	private WaitForSeconds waitTime = new WaitForSeconds(0.15f);
+
+	void OnCollisionEnter(Collision other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			StartCoroutine (PlayCoinSound()); // invoke coroutine
+		}
+	}
+
+	// Coroutine
+	IEnumerator PlayCoinSound() {
+		this.CoinSound.Play ();
+		yield return this.waitTime;
+		this.gameObject.SetActive (false);
+	}
+
+	void Update() {
+		this.gameObject.transform.Rotate (Vector3.forward * Time.deltaTime * 360);
+	}
+}
---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public Transform SpawnPoint;
+	public GameObject Player; // player prefab
+	public GameObject Coin; // coin prefab
+	public int MaxCoins = 20;
+
+	private List<GameObject> CoinPool; // reference to the CoinPool List
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (this.Player);
+		this.Player.transform.position = SpawnPoint.position; // spawn the player at the SpawnPoint
+		this.CoinPool = new List<GameObject>(); // initialize the CoinPool
+		this.BuildCoinPool();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.PlaceCoins ();
+	}
+
+	private void BuildCoinPool() {
+		for (int countCount = 0; countCount < this.MaxCoins; countCount++) {
+			GameObject coin = (GameObject)Instantiate (this.Coin);
+			coin.SetActive (false);
+			this.CoinPool.Add (coin);
+		}
+	}
+
+	private void PlaceCoins() {
+		foreach (var coin in CoinPool) {
+			if (!coin.activeInHierarchy) { // search the pool for a coin that is not in the scene
+				coin.SetActive (true); // place the coin in the scene
+				coin.transform.position = new Vector3 (Random.Range (-20f, 20f), 20, Random.Range (-20f, 20f));
+			}
+		}
+	}
+}
